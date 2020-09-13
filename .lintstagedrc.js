b38b4e8420bfe81917845ddfc0033b5b7e368e8c
liangxinwei <sivan.shanghai@gmail.com>
2020-09-13 23:00:07 +0800

    update: add husky

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,12 +1,10 @@
 module.exports = {
   'src/**/*.{json,css,scss,md,mdx}': [
     'prettier --write',
-    'git add'
   ],
   'src/**/*.{js,jsx,ts,tsx}': [
     'eslint --fix',
     'stylelint --allow-empty-input --fix',
     'prettier --write',
-    'git add',
   ],
 }
--- commitlint.config.js
+++ commitlint.config.js
@@ -3,7 +3,6 @@ const types = ['add', 'update', 'remove', 'fix', 'docs']
 module.exports = {
   rules: {
     'scope-case': [2, 'always', 'lower-case'],
-    'scope-empty': [2, 'never'],
     'subject-empty': [2, 'never'],
     'type-case': [2, 'always', 'lower-case'],
     'type-empty': [2, 'never'],
